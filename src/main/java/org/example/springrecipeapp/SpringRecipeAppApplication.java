dd68e20e63b8ffaefd8bd7244f7a7096f329f61a
Nishant Yadav <nishantyadav025@gmail.com>
2024-08-01 18:14:50 -0700

    Initialize Spring Boot Project

---
+++ src/main/java/org/example/springrecipeapp/SpringRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
